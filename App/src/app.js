c7f7e4a276f0572e9ee8d2902b0928bb7527b7d0
joey-squla <joey@squla.com>
2018-02-07 11:20:17 +0100

    Added update loops

--- App/src/app.js
+++ App/src/app.js
@@ -1,3 +1,2 @@
 import bar from './bar';
-import phaser from './vendor/phaser'
 bar();
--- App/src/bar.js
+++ App/src/bar.js
@@ -1,15 +1,16 @@
-export default function bar() {
-    console.log("Allah Snackbar!");
+import phaser from './vendor/phaser'
 
+export default function bar() {
     var config = {
         width: 800,
         height: 600,
         resolution: 1,
         type: Phaser.WEBGL,
         parent: '',
-        state: {
+        scene: {
             preload: preload,
-            create: create
+            create: create,
+            update:update
         },
         callbacks: {
             preBoot: function () { console.log('I get called before all of the Game systems are created, but after Device is available')},
@@ -19,6 +20,9 @@ export default function bar() {
 
     var game = new Phaser.Game(config);
 
+    function update(){
+        console.log("update");
+    }
 
     function preload(){
         console.log("PRELOAD");
@@ -27,8 +31,6 @@ export default function bar() {
     function create(){
         console.log("createa");
     }
-    console.log("RUN!:" + game);
-    console.log(game);
 }
 
 
